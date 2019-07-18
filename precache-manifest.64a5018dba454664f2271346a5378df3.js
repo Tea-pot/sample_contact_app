01c5f3217668a01b0b36a8112a913f8ec2463063
Kamil Kowalczyk <kppkowalczyk@gmail.com>
2019-07-18 04:58:28 +0300

    Updates

---
+++ precache-manifest.64a5018dba454664f2271346a5378df3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009acf7f510be2ca84be82cf9a565123",
+    "url": "/sample_contact_app/index.html"
+  },
+  {
+    "revision": "e5ae164b2e9723dfd008",
+    "url": "/sample_contact_app/static/css/2.fc695b0e.chunk.css"
+  },
+  {
+    "revision": "23c27154805a7c17adaf",
+    "url": "/sample_contact_app/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e5ae164b2e9723dfd008",
+    "url": "/sample_contact_app/static/js/2.dbf0cb78.chunk.js"
+  },
+  {
+    "revision": "23c27154805a7c17adaf",
+    "url": "/sample_contact_app/static/js/main.4b62f687.chunk.js"
+  },
+  {
+    "revision": "4b680d3b0d18707ec58f",
+    "url": "/sample_contact_app/static/js/runtime~main.9e02cb78.js"
+  }
+]);
